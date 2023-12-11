1693d07d98e126d5fcdeed63ebb3fab34deacbc4
YangyaseniNayak <msyangyaseni@gmail.com>
2023-12-11 10:46:01 +0530

    new

---
+++ DTO/Offoring_CFRPDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Onnes.DTO
+{
+  
+        public class AddOfforing_CFRP
+        {       
+            public string content { get; set; }
+        }
+}
---
+++ Model/Offoring_CFRP.cs
@@ -0,0 +1,8 @@
+﻿namespace Onnes.Model
+{
+    public class Offoring_CFRP
+    {
+        public int Id { get; set; }
+        public string content { get; set; }
+    }
+}
